dff00ec8af65bfb7e371c1a1615fd8337e5734bf
Alexander <kalmykovalexander28@gmail.com>
2021-07-09 17:40:09 +0300

    Add model Stage

--- tool_app/models.py
+++ tool_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
+class Stage(models.Model):
+    stage = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.stage
+
+
 class Tool(models.Model):
     name = models.CharField(max_length=150)
-    stage = models.IntegerField(default=1)
-    version = models.CharField(max_length=100)
+    stage = models.ForeignKey(Stage, default=1, on_delete=models.CASCADE)
+    version = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
